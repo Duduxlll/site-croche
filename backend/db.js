36256fa21cf2c33f854da16e23539c0301b3260a
Dudullll <vitoreduar05@gmail.com>
2025-06-24 14:08:51 -0300

    Corrigindo conexão com banco do Render

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,11 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-
-    connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
